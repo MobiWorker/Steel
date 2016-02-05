4f4417033daa0bd54f49396197d452de0be8394f
Mariotaku Lee <mariotaku.lee@gmail.com>
2016-02-05 13:42:51 +0800

    improved network state check

--- src/main/java/edu/tsinghua/location/research/NetworkReceiver.java
+++ src/main/java/edu/tsinghua/location/research/NetworkReceiver.java
@@ -5,27 +5,26 @@ import android.content.Context;
 import android.content.Intent;
 import android.net.ConnectivityManager;
 import android.os.AsyncTask;
+import android.support.v4.net.ConnectivityManagerCompat;
 
 import edu.tsinghua.hotmobi.HotMobiLogger;
 import edu.tsinghua.hotmobi.UploadLogsTask;
 import edu.tsinghua.location.research.util.Utils;
 
 public class NetworkReceiver extends BroadcastReceiver {
-    public NetworkReceiver() {
-    }
 
     @Override
     public void onReceive(Context context, Intent intent) {
         if (!ConnectivityManager.CONNECTIVITY_ACTION.equals(intent.getAction())) return;
-
-        final int networkType = Utils.getActiveNetworkType(context.getApplicationContext());
-        final boolean isWifi = networkType == ConnectivityManager.TYPE_WIFI;
-        final boolean isCharging = Utils.isCharging(context.getApplicationContext());
-        if (isWifi && isCharging) {
+        final Context appContext = context.getApplicationContext();
+        final ConnectivityManager cm = (ConnectivityManager) appContext.getSystemService(Context.CONNECTIVITY_SERVICE);
+        final boolean isMetered = ConnectivityManagerCompat.isActiveNetworkMetered(cm);
+        final boolean isCharging = Utils.isCharging(appContext);
+        if (!isMetered && isCharging) {
             final long currentTime = System.currentTimeMillis();
             final long lastSuccessfulTime = HotMobiLogger.getLastUploadTime(context);
             if ((currentTime - lastSuccessfulTime) > HotMobiLogger.UPLOAD_INTERVAL_MILLIS) {
-                AsyncTask.execute(new UploadLogsTask(context.getApplicationContext()));
+                AsyncTask.execute(new UploadLogsTask(appContext));
             }
         }
     }
